4f4e3aa9147b03b82587d975a2510120721d8b45
anujGit11 <159407932+anujGit11@users.noreply.github.com>
2025-07-02 14:18:44 +0530

    Update Test5Application.java

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
+
+		int a = 13;
+		char b = 'a';
+		String c = "anuj";
 		SpringApplication.run(Test5Application.class, args);
 	}
 
